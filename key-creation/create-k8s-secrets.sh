f7dd1e3bc3459175b3fb2d983d3d3b5e932c5ce5
Christian Woehrle <christian.woehrle@dwpbank.de>
2018-12-30 09:17:10 +0100

    a little bit less brittle

--- key-creation/create-k8s-secrets.sh
+++ key-creation/create-k8s-secrets.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd $(dirname $0)
+
 kubectl create secret tls admissioncontroller --key admissioncontroller.key --cert admissioncontroller.crt
 
 kubectl create secret generic ca-secret --from-file=ca.crt=ca.crt 
